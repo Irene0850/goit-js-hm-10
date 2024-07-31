32d87f843d1050dcf66454101821f630fed4bcf2
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-07-31 22:01:43 +0300

    new

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,39 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+document.querySelector('.form').addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('[name="delay"]');
+  const stateInput = document.querySelector('[name="state"]:checked');
+
+  const delay = parseInt(delayInput.value, 10);
+
+  try {
+    const result = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (stateInput.value === 'fulfilled') {
+          resolve(`✅ Fulfilled promise in ${delay} ms`);
+        } else {
+          reject(`❌ Rejected promise in ${delay} ms`);
+        }
+      }, delay);
+
+      delayInput.value = '';
+    });
+
+    iziToast.settings({
+      icon: '',
+    });
+
+    iziToast.success({
+      message: result,
+      position: 'topRight',
+    });
+  } catch (error) {
+    iziToast.error({
+      message: error,
+      position: 'topRight',
+    });
+  }
+});
